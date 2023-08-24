beb2b3c5ceeb1395260cb7a5bfab4ae29d1be611
Amirreza <amirreza.m13825@gmail.com>
2023-08-24 21:49:42 +0330

    fetch courses in a different way (in a client component)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,31 @@
+"use client";
+
 import Courses from "@/components/Courses";
+import { useState, useEffect, Suspense } from "react";
+import LoadingPage from "./loading";
 
 export default function Home() {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const response = await fetch("/api/courses");
+      const courses = await response.json();
+
+      setCourses(courses);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) return <LoadingPage />;
+
   return (
     <>
       <h1>Welcome to Next.js V13</h1>
-      <Courses />
+      <Courses courses={courses} />
     </>
   );
 }
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,28 +1,21 @@
 import Link from "next/link";
 
-async function fetchCourses() {
-  const response = await fetch("http://localhost:3000/api/courses");
-  const courses = await response.json();
-  return courses;
-}
-
-export default async function Courses() {
-  const courses = await fetchCourses();
-
+export default async function Courses({ courses }) {
   return (
     <div className="courses">
-      {courses.map((course) => {
-        return (
-          <div className="card" key={course.id}>
-            <h2>{course.title}</h2>
-            <small>Level: {course.level}</small>
-            <p>{course.description}</p>
-            <Link href={course.link} target="_blank" className="btn">
-              Go To Course
-            </Link>
-          </div>
-        );
-      })}
+      {courses &&
+        courses.map((course) => {
+          return (
+            <div className="card" key={course.id}>
+              <h2>{course.title}</h2>
+              <small>Level: {course.level}</small>
+              <p>{course.description}</p>
+              <Link href={course.link} target="_blank" className="btn">
+                Go To Course
+              </Link>
+            </div>
+          );
+        })}
     </div>
   );
 }
