1b55c60c44ab2544eb6f69ec3a9b7260c92df750
Amirreza <amirreza.m13825@gmail.com>
2023-08-24 20:17:32 +0330

    create Repo and RepoDirs components

---
+++ src/components/Repo.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+
+async function fetchRepo(repoName) {
+  const response = await fetch(
+    `https://api.github.com/repos/am1rreza/${repoName}`
+  );
+  const repo = await response.json();
+  return repo;
+}
+
+export default async function Repo({ repoName }) {
+  const repo = await fetchRepo(repoName);
+
+  return (
+    <>
+      <h2>{repo.name}</h2>
+      <p>{repo.description}</p>
+      <div className="card-stats">
+        <div className="card-stat">
+          <FaStar />
+          <span>{repo.stargazers_count}</span>
+        </div>
+        <div className="card-stat">
+          <FaCodeBranch />
+          <span>{repo.forks_count}</span>
+        </div>
+        <div className="card-stat">
+          <FaEye />
+          <span>{repo.watchers_count}</span>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/RepoDirs.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+async function fetchRepoContents(repoName) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const response = await fetch(
+    `https://api.github.com/repos/am1rreza/${repoName}/contents`
+  );
+  const contents = await response.json();
+  return contents;
+}
+
+export default async function RepoDirs({ repoName }) {
+  const contents = await fetchRepoContents(repoName);
+  const dirs = contents.filter(({ type }) => type === "dir");
+
+  return (
+    <>
+      <h3>Directories</h3>
+      <ul>
+        {dirs.map((dir) => (
+          <li key={dir.path}>
+            <Link href={`/code/repos/${repoName}/${dir.path}`}>{dir.path}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
