7ff2c2272295f48643bb99d8fde6b9bb8c235bb0
Amirreza <amirreza.m13825@gmail.com>
2023-08-24 21:00:03 +0330

    create the courses component and fetch courses from Next API route

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,10 @@
-import Link from "next/link";
+import Courses from "@/components/Courses";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <h1>Welcome to Next.js V13</h1>
-      <ul>
-        <li>
-          <Link href={"/"}>Home</Link>
-        </li>
-        <li>
-          <Link href={"/about"}>About</Link>
-        </li>
-        <li>
-          <Link href={"/about/team"}>Team</Link>
-        </li>
-      </ul>
-    </div>
+      <Courses />
+    </>
   );
 }
