6b3d1fc1fcd76341ec9d5f5f73eb05d5f909cc48
Amirreza <amirreza.m13825@gmail.com>
2023-08-24 20:21:14 +0330

    add Suspense component for better user experince

--- src/app/code/repos/[repoName]/page.jsx
+++ src/app/code/repos/[repoName]/page.jsx
@@ -1,6 +1,7 @@
 import Repo from "@/components/Repo";
 import RepoDirs from "@/components/RepoDirs";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function RepoPage({ params }) {
   return (
@@ -8,8 +9,12 @@ export default function RepoPage({ params }) {
       <Link className="btn btn-back" href={"/code/repos"}>
         Back to Repositories
       </Link>
-      <Repo repoName={params.repoName} />
-      <RepoDirs repoName={params.repoName} />
+      <Suspense fallback={<h4>Loading Repo...</h4>}>
+        <Repo repoName={params.repoName} />
+      </Suspense>
+      <Suspense fallback={<h4>Loading Directories...</h4>}>
+        <RepoDirs repoName={params.repoName} />
+      </Suspense>
     </div>
   );
 }
